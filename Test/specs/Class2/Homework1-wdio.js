0ba1f2d8efa6cdca024344202b474ed6cbc8c9fa
Janery <janery@gmail.com>
2022-11-20 23:25:52 -0500

    re submitting Homework1-wdio

--- Test/specs/Class2/Homework1-wdio.js
+++ Test/specs/Class2/Homework1-wdio.js
@@ -1,26 +1,80 @@
-// Verify male-gender button is not selected
-const maleRadioButton = await $('input[value="1"]');
-const ismaleGenderSelected = await maleRadioButton.isSelected();
-expect(ismaleGenderSelected, 'male gender is already selected').to.be.false;
+const {expect} = require("chai");
+const {it} = require("mocha");
 
-await browser.pause(7000)
 
-// Verify custom-gender button is not selected
-const customGenderRadioButton = await $('input[value="1"]');
-const iscustomGenderSelected = await customGenderRadioButton.isSelected();
-expect(iscustomGenderSelected, 'custom gender is already selected').to.be.false;
+describe('Homework - 1' , () => {
 
-await browser.pause(7000)
+it('Testcase homework1', async () => {
+    /**
+     * Steps:
+     * 1. Launch facebook.com
+     * 2. Click Create New Account
+     * 3. Verify female-gender button is not selected
+     * 4. Verify male-gender button is not selected
+     * 5. Verify custom-gender button is not selected
+     * 6. If female gender is NOT selected, then click on female gender radio button
+     * 7. Verify female-gender button is selected
+     */
 
-//If female gender is NOT selected, then click on female gender radio button
-    if(femaleRadioButton = await $('input[value="1"]')){
+    // 1. Launch facebook.com
+    await browser.url('/');
+
+    // 2. Click Create New Account button
+    const createNewAccountButton = await $('=Create new account');
+    createNewAccountButton.click();
+
+    await browser.pause(2000);
+
+    // 3. Verify female-gender button is not selected
+    const femaleRadioButton = await $('input[value="1"]');
     const isFemaleGenderSelected = await femaleRadioButton.isSelected();
     expect(isFemaleGenderSelected, 'Female gender is already selected').to.be.false;
+
+    await browser.pause(2000)
+
+    // Verify male-gender button is not selected
+    const maleRadioButton = await $('input[value="2"]');
+    const ismaleGenderSelected = await maleRadioButton.isSelected();
+    expect(ismaleGenderSelected, 'male gender is already selected').to.be.false;
+
+    await browser.pause(2000)
+
+    // Verify custom-gender button is not selected
+    const customGenderRadioButton = await $('input[value="-1"]');
+    const iscustomGenderSelected = await customGenderRadioButton.isSelected();
+    expect(iscustomGenderSelected, 'custom gender is already selected').to.be.false;
+
+    await browser.pause(2000)
+
+    //If female gender is NOT selected, then click on female gender radio button
+    if (!isFemaleGenderSelected) {              // isFemaleGenderSelected === false     <--> !isFemaleGenderSelected
+    await femaleRadioButton.click()
     }
-await browser.pause(7000)
+   
+    
+    await browser.pause(2000)
 
 
-// Verify female-gender button is selected
-const femaleRadioButton = await $('input[value="1"]');
-const isFemaleGenderSelected = await femaleRadioButton.isSelected();
-expect(isFemaleGenderSelected, 'Female gender is not selected').to.be.true;
+    // Verify female-gender button is selected
+    //const FmaleRadioButton = await $('input[value="1"]');
+    //const isFmaleGenderSelected = await FmaleRadioButton.isSelected();
+    //expect(isFmaleGenderSelected, 'Female gender is not selected').to.be.true;
+
+    if (!isFemaleGenderSelected) {
+        //click female gender button
+        femaleRadioButton.click();
+        //check if button is selected
+        const isFemaleGenderSelected = await femaleRadioButton.isSelected();
+        expect(isFemaleGenderSelected, "Female gender is already selected").to.be
+        .true;}
+        await browser.pause(5000);
+});
+
+
+});
+
+
+
+
+
+
